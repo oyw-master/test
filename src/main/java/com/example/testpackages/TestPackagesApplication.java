8b9f43fde2b44bb22370d9a22c5d8734ff5df298
oyw-master <149041897+oyw-master@users.noreply.github.com>
2023-12-07 15:54:14 +0800

    Add files via upload

---
+++ src/main/java/com/example/testpackages/TestPackagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpackages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPackagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPackagesApplication.class, args);
+    }
+
+}
